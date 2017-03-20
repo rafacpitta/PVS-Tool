d477f328b8c48be52af89c4778d61f38ec543f37
Rafael Pitta <rafael_pitta@hotmail.com>
2017-03-20 06:47:54 -0300

    Critical values writing

--- plotting.py
+++ plotting.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
 import mdfreader as mdf
 from scipy.stats import linregress
 import os
@@ -16,7 +15,7 @@ def read_files():
 	
 	os.chdir('Files')
 	for i in filenames:
-		f = mdf.mdf(i,convertAfterRead=False)
+		f = mdf.mdf(i)
 		files.append(f)
 	os.chdir('..')
 	
@@ -29,13 +28,14 @@ def regression(x,y,f):
 	temp = 100*trend.intercept
 	print '\ny = (' + str(trend.slope) + ') * x + (' + str(trend.intercept) + ')\tfor ' + f
 
-def plotting_xy(data):
+def plotting_xy(data,x,y):
+	
 	for i in xrange(len(data)):
 	
 		try:
-			plt.plot(data[i][x_str]['data'], data[i][y_str]['data'], 'bo', ms = '0.7')
-			plt.xlabel(x_str + '(' + str(data[i][x_str]['unit']) + ')')
-			plt.ylabel(y_str + '(' + str(data[i][y_str]['unit']) + ')')
+			plt.plot(data[i][x]['data'], data[i][y]['data'], 'bo', ms = '0.7')
+			plt.xlabel(x + '(' + data[i][x]['unit'] + ')')
+			plt.ylabel(y + '(' + data[i][y]['unit'] + ')')
 			plt.show()
 	
 			regression(data[i][x_str]['data'], data[i][y_str]['data'], arquivos[i])
@@ -43,15 +43,27 @@ def plotting_xy(data):
 			print 'Variables with different raster'
 			sys.exit()
 
-while True:
-	dat, arquivos = read_files()
+#while True:
+data, arquivos = read_files()
 
-	x_str = str(raw_input('\nType X axis variable: ')).upper()
-	y_str = str(raw_input('Type Y axis variable: ')).upper()
+x_str = str(raw_input('\nType X axis variable: ')).upper()
+y_str = str(raw_input('Type Y axis variable: ')).upper()
 
-	plotting_xy(dat)
+data[0].resample()
+crit = []
+j = 0
+fcrit = open('critical.txt','w')
+
+for i in xrange(data[0][x_str]['data'].shape[0]):
+	if (1.33 < data[0][x_str]['data'][i] < 1.36) and (0.50 < data[0][y_str]['data'][i] < 0.52):
+		crit.append(i)
+		j += 1
+		fcrit.write(str(i) + '\n')
+
+fcrit.close()
+plotting_xy(data,x_str,y_str)
 	
-	while True:
+"""	while True:
 		choice = str(raw_input('\nDo you want to plot again? (Y/N) ')).lower()
 		
 		if choice == 'n':
@@ -62,5 +74,6 @@ while True:
 			
 		else:
 			print '\nAnswer just y for YES or n for NO!'
-			continue
+			continue"""
+
 
